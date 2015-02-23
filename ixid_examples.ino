d76693cc8098287a638f91b75dfd0f5a945d0879
Steve Faletti <stevefaletti@gmail.com>
2015-02-23 17:35:18 -0500

    Analog Input
    - reads a pot and prints that value to the Serial port
    - uses a loop delay function for stability

--- ixid_examples.ino
+++ ixid_examples.ino
@@ -1,57 +1,27 @@
-#include "Bounce2.h"
+//polls analog pin with a pot or similar
+//prints the analog value to the Serial port
 
-// declare pins
-const int buttonPin_1 = 3;
-const int buttonPin_2 = 4;
-const int buttonPin_3 = 5;
+//declare pin values 
+const int potPin = 0;
 const int ledPin_1 = 10;
 const int ledPin_2 = 11;
 
-// declare input value variables
-boolean ledVal_1 = false;
-boolean ledVal_2 = false;
-boolean switchVal = true;
-
-// declare button object for easier edge detection
-Bounce button_1 = Bounce();
-Bounce button_switch = Bounce();
+//declare variables
+int potVal = 0;
 
 void setup()
 {
-	pinMode(buttonPin_1, INPUT_PULLUP);
-	pinMode(buttonPin_2, INPUT_PULLUP);
-	pinMode(buttonPin_3, INPUT_PULLUP);
+	Serial.begin(9600); //start Serial
+
 	pinMode(ledPin_1, OUTPUT);
 	pinMode(ledPin_2, OUTPUT);
-
-	// set up Bounce object
-	// digitalWrite(buttonPin_1, HIGH);
-	button_1.attach(buttonPin_1);
-	button_switch.attach(buttonPin_3);
-	button_1.interval(5);
-	button_switch.interval(5);
 }
 
 void loop()
 {
-	ledVal_2 = !digitalRead(buttonPin_2);
+	//read analog pin with potentiometer
+	potVal = analogRead(potPin);
+	Serial.println(potVal);
 
-	button_1.update();
-	if (button_1.fell()) {
-		ledVal_1 = !ledVal_1;
-	}
-
-	button_switch.update();
-	if (button_switch.fell()) {
-		switchVal = !switchVal;
-	}
-
-	if (switchVal) { 
-		digitalWrite(ledPin_1, ledVal_1);
-		digitalWrite(ledPin_2, ledVal_2);
-	}
-	else {
-		digitalWrite(ledPin_2, ledVal_1);
-		digitalWrite(ledPin_1, ledVal_2);
-	}
+	delay(10); //delay keeps things more stable
 }
