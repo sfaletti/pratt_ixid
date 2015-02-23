60765fb9d9159763eb8065f257ad12c212ed2d5a
Steve Faletti <stevefaletti@gmail.com>
2015-02-23 17:50:00 -0500

    Hybrid Input & Digital Output
    - Reads potpin to control LED brightness
    - LEDs are set in opposition to one another
    - puchbutton on pin 5 switches LED brightness values

--- ixid_examples.ino
+++ ixid_examples.ino
@@ -2,13 +2,18 @@
 //prints the analog value to the Serial port
 //controls LEDs based on pot value
 
+#include "Bounce2.h" //use Bounce library for button control
+
 //declare pin values 
 const int potPin = 0;
 const int ledPin_1 = 10;
 const int ledPin_2 = 11;
+const int buttonPin_1 = 5;
 
 //declare variables
 int potVal = 0;
+boolean toggleVal = false;
+Bounce toggle_button = Bounce();
 
 void setup()
 {
@@ -16,6 +21,12 @@ void setup()
 
 	pinMode(ledPin_1, OUTPUT);
 	pinMode(ledPin_2, OUTPUT);
+
+	//set up button. Pin first, then object
+	pinMode(buttonPin_1, INPUT_PULLUP);
+	toggle_button.attach(buttonPin_1);
+	toggle_button.interval(5);
+
 }
 
 void loop()
@@ -26,9 +37,21 @@ void loop()
 	
 	Serial.println(potVal); //write converted value to Serial port
 
-	//convert pot value to 8 bit number and write to an LED
-	analogWrite(ledPin_2, potVal);
-	analogWrite(ledPin_1, 255 - potVal);
+	//check toggle button
+	toggle_button.update();
+	if (toggle_button.fell()) {
+		toggleVal = !toggleVal;
+	}
+
+	if (toggleVal) { 
+	//write converted pot value to LED
+		analogWrite(ledPin_2, potVal);
+		analogWrite(ledPin_1, 255 - potVal);
+	}
+	else {
+		analogWrite(ledPin_2, 255-potVal);
+		analogWrite(ledPin_1, potVal);
+	}
 
 	delay(10); //delay keeps things more stable
 }
