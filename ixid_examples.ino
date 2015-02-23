5637d9418e543bce8f2fd0a3e98149d1341f8da1
Steve Faletti <stevefaletti@gmail.com>
2015-02-23 17:41:26 -0500

    Analog Input & Output
    - Reads potentiometer.
    - Converts pot value to 8-bit length
    - Writes pot value and inverse to two LEDS, so they dim in opposition to each other.

--- ixid_examples.ino
+++ ixid_examples.ino
@@ -1,5 +1,6 @@
 //polls analog pin with a pot or similar
 //prints the analog value to the Serial port
+//controls LEDs based on pot value
 
 //declare pin values 
 const int potPin = 0;
@@ -19,9 +20,15 @@ void setup()
 
 void loop()
 {
-	//read analog pin with potentiometer
-	potVal = analogRead(potPin);
-	Serial.println(potVal);
+	
+	potVal = analogRead(potPin); //read analog pin with potentiometer
+	potVal /= 4; //convert from 10-bit to 8-bit value
+	
+	Serial.println(potVal); //write converted value to Serial port
+
+	//convert pot value to 8 bit number and write to an LED
+	analogWrite(ledPin_2, potVal);
+	analogWrite(ledPin_1, 255 - potVal);
 
 	delay(10); //delay keeps things more stable
 }
