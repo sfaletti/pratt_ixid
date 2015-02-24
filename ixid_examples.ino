e3c0fd3ec53ba75218ad4dea7312a78cfbf9cbbb
Steve Faletti <stevefaletti@gmail.com>
2015-02-24 11:20:14 -0500

    RFID - Mifare Reader
    - Sparkfun example code to read MiFare tags.
    - Does NOT write to tags.
    - Uses Sparkfun Evaluation Board.
    - Code available at https://github.com/sparkfun/RFID_Evaluation_Shield

--- ixid_examples.ino
+++ ixid_examples.ino
@@ -1,88 +1,147 @@
-#include <Wire.h>
-#include "Adafruit_TCS34725.h"
+/*
+  RFID Eval 13.56MHz Shield example sketch v10
 
-// Pick analog outputs, for the UNO these three work well
-// use ~560  ohm resistor between Red & Blue, ~1K for green (its brighter)
-#define redpin 12
-#define greenpin 14
-#define bluepin 15
-// for a common anode LED, connect the common pin to +5V
-// for common cathode, connect the common to ground
+  Aaron Weiss, aaron at sparkfun dot com
+  OSHW license: http://freedomdefined.org/OSHW
 
-// set to false if using a common cathode LED
-#define commonAnode true
+  works with 13.56MHz MiFare 1k tags
 
-// our RGB -> eye-recognized gamma color
-byte gammatable[256];
+  Based on hardware v13:
+  D7 -> RFID RX
+  D8 -> RFID TX
+  D9 -> XBee TX
+  D10 -> XBee RX
 
+  Note: RFID Reset attached to D13 (aka status LED)
 
-Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+  Note: be sure include the SoftwareSerial lib, http://arduiniana.org/libraries/newsoftserial/
 
-void setup() {
+  Usage: Sketch prints 'Start' and waits for a tag. When a tag is in range, the shield reads the tag,
+  blinks the 'Found' LED and prints the serial number of the tag to the serial port
+  and the XBee port.
+  
+  06/04/2013 - Modified for compatibility with Arudino 1.0. Seb Madgwick.
+
+*/
+#include <SoftwareSerial.h>
+
+SoftwareSerial rfid(7, 8);
+SoftwareSerial xbee(10, 9);
+
+//Prototypes
+void check_for_notag(void);
+void halt(void);
+void parse(void);
+void print_serial(void);
+void read_serial(void);
+void seek(void);
+void set_flag(void);
+
+//Global var
+int flag = 0;
+int Str1[11];
+
+//INIT
+void setup()
+{
   Serial.begin(9600);
-  Serial.println("Color View Test!");
+  Serial.println("Start");
 
-  if (tcs.begin()) {
-    Serial.println("Found sensor");
-  } else {
-    Serial.println("No TCS34725 found ... check your connections");
-    while (1); // halt!
+  // set the data rate for the SoftwareSerial ports
+  xbee.begin(9600);
+  rfid.begin(19200);
+  delay(10);
+  halt();
+}
+
+//MAIN
+void loop()
+{
+  read_serial();
+}
+
+void check_for_notag()
+{
+  seek();
+  delay(10);
+  parse();
+  set_flag();
+
+  if(flag = 1){
+    seek();
+    delay(10);
+    parse();
   }
-  
-  // use these three pins to drive an LED
-  pinMode(redpin, OUTPUT);
-  pinMode(greenpin, OUTPUT);
-  pinMode(bluepin, OUTPUT);
-  
-  // thanks PhilB for this gamma table!
-  // it helps convert RGB colors to what humans see
-  for (int i=0; i<256; i++) {
-    float x = i;
-    x /= 255;
-    x = pow(x, 2.5);
-    x *= 255;
-      
-    if (commonAnode) {
-      gammatable[i] = 255 - x;
-    } else {
-      gammatable[i] = x;      
+}
+
+void halt()
+{
+ //Halt tag
+  rfid.write((uint8_t)255);
+  rfid.write((uint8_t)0);
+  rfid.write((uint8_t)1);
+  rfid.write((uint8_t)147);
+  rfid.write((uint8_t)148);
+}
+
+void parse()
+{
+  while(rfid.available()){
+    if(rfid.read() == 255){
+      for(int i=1;i<11;i++){
+        Str1[i]= rfid.read();
+      }
     }
-    //Serial.println(gammatable[i]);
   }
 }
 
-
-void loop() {
-  uint16_t clear, red, green, blue;
-
-  tcs.setInterrupt(false);      // turn on LED
-
-  delay(60);  // takes 50ms to read 
-  
-  tcs.getRawData(&red, &green, &blue, &clear);
-
-  tcs.setInterrupt(true);  // turn off LED
-  
-  Serial.print("C:\t"); Serial.print(clear);
-  Serial.print("\tR:\t"); Serial.print(red);
-  Serial.print("\tG:\t"); Serial.print(green);
-  Serial.print("\tB:\t"); Serial.print(blue);
-
-  // Figure out some basic hex code for visualization
-  uint32_t sum = clear;
-  float r, g, b;
-  r = red; r /= sum;
-  g = green; g /= sum;
-  b = blue; b /= sum;
-  r *= 256; g *= 256; b *= 256;
-  Serial.print("\t");
-  Serial.print((int)r, HEX); Serial.print((int)g, HEX); Serial.print((int)b, HEX);
-  Serial.println();
-
-  //Serial.print((int)r ); Serial.print(" "); Serial.print((int)g);Serial.print(" ");  Serial.println((int)b );
-
-  analogWrite(redpin, gammatable[(int)r]);
-  analogWrite(greenpin, gammatable[(int)g]);
-  analogWrite(bluepin, gammatable[(int)b]);
+void print_serial()
+{
+  if(flag == 1){
+    //print to serial port
+    Serial.print(Str1[8], HEX);
+    Serial.print(Str1[7], HEX);
+    Serial.print(Str1[6], HEX);
+    Serial.print(Str1[5], HEX);
+    Serial.println();
+    //print to XBee module
+    xbee.print(Str1[8], HEX);
+    xbee.print(Str1[7], HEX);
+    xbee.print(Str1[6], HEX);
+    xbee.print(Str1[5], HEX);
+    xbee.println();
+    delay(100);
+    //check_for_notag();
+  }
 }
 
+void read_serial()
+{
+  seek();
+  delay(10);
+  parse();
+  set_flag();
+  print_serial();
+  delay(100);
+}
+
+void seek()
+{
+  //search for RFID tag
+  rfid.write((uint8_t)255);
+  rfid.write((uint8_t)0);
+  rfid.write((uint8_t)1);
+  rfid.write((uint8_t)130);
+  rfid.write((uint8_t)131);
+  delay(10);
+}
+
+void set_flag()
+{
+  if(Str1[2] == 6){
+    flag++;
+  }
+  if(Str1[2] == 2){
+    flag = 0;
+  }
+}
