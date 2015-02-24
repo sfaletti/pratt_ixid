3e9488669d7598199f189009eceec49111cf2f3c
Steve Faletti <stevefaletti@gmail.com>
2015-02-23 20:49:31 -0500

    POV
    - runs low-frequency PWM in software on pin10.
    - runs an LED on pin11 HIGH as comparison.

--- ixid_examples.ino
+++ ixid_examples.ino
@@ -1,57 +1,36 @@
-//polls analog pin with a pot or similar
-//prints the analog value to the Serial port
-//controls LEDs based on pot value
-
-#include "Bounce2.h" //use Bounce library for button control
-
 //declare pin values 
 const int potPin = 0;
-const int ledPin_1 = 10;
-const int ledPin_2 = 11;
-const int buttonPin_1 = 5;
+const int pwmLEDPin = 10;
+const int onLEDPin = 11;
 
 //declare variables
-int potVal = 0;
-boolean toggleVal = false;
-Bounce toggle_button = Bounce();
+unsigned long lastMillis = 0;
+int elapsedTime = 0;
+
+float dutyCycle = .5;
+float freq = 1000.0 / 80;
+
+boolean pwmVal = true;
+boolean onVal = true;
 
 void setup()
 {
-	Serial.begin(9600); //start Serial
-
-	pinMode(ledPin_1, OUTPUT);
-	pinMode(ledPin_2, OUTPUT);
-
-	//set up button. Pin first, then object
-	pinMode(buttonPin_1, INPUT_PULLUP);
-	toggle_button.attach(buttonPin_1);
-	toggle_button.interval(5);
-
+	pinMode(pwmLEDPin, OUTPUT);
+	pinMode(onLEDPin, OUTPUT);
+	digitalWrite(onLEDPin, onVal);
 }
 
 void loop()
 {
+	dutyCycle = float(analogRead(potPin)) / 1023.0;
+
+	elapsedTime = millis() - lastMillis;
 	
-	potVal = analogRead(potPin); //read analog pin with potentiometer
-	potVal /= 4; //convert from 10-bit to 8-bit value
-	
-	Serial.println(potVal); //write converted value to Serial port
-
-	//check toggle button
-	toggle_button.update();
-	if (toggle_button.fell()) {
-		toggleVal = !toggleVal;
+	if (elapsedTime > freq) { //reset time counter
+		lastMillis = millis();
 	}
 
-	if (toggleVal) { 
-	//write converted pot value to LED
-		analogWrite(ledPin_2, potVal);
-		analogWrite(ledPin_1, 255 - potVal);
-	}
-	else {
-		analogWrite(ledPin_2, 255-potVal);
-		analogWrite(ledPin_1, potVal);
-	}
+	pwmVal = elapsedTime < freq * dutyCycle;
 
-	delay(10); //delay keeps things more stable
+	digitalWrite(pwmLEDPin, pwmVal);
 }
