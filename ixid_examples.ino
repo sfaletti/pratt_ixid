6c425d92be4b0aeea9b794d2ec32861b6b2ff8e8
Steve Faletti <stevefaletti@gmail.com>
2015-02-24 10:48:17 -0500

    Color Sensor with RGB
    - Copied in from Adafruit_TCS34725 example file.
    - Changed RGB LED pins to work with Teensy 2.0.
    - RGB Color Sensor library can be found at https://github.com/adafruit/Adafruit_TCS34725

--- ixid_examples.ino
+++ ixid_examples.ino
@@ -1,40 +1,88 @@
-//declare pin values 
-const int accelX = 0;
-const int accelY = 1;
-const int accelZ = 2;
+#include <Wire.h>
+#include "Adafruit_TCS34725.h"
 
-const int gyroX = 3;
-const int gyroY = 4;
-const int gyroZ = 5;
+// Pick analog outputs, for the UNO these three work well
+// use ~560  ohm resistor between Red & Blue, ~1K for green (its brighter)
+#define redpin 12
+#define greenpin 14
+#define bluepin 15
+// for a common anode LED, connect the common pin to +5V
+// for common cathode, connect the common to ground
+
+// set to false if using a common cathode LED
+#define commonAnode true
+
+// our RGB -> eye-recognized gamma color
+byte gammatable[256];
 
 
-//declare variables
-const int sensorCount = 6; //convenience variable
-int sensorVals[sensorCount]; //array to store 6DOF sensor values
-const int sensorPins[sensorCount] = {accelX, accelY, accelZ, gyroX, gyroY, gyroZ}; //array to store pin values. This will be easier to process
-String pinNames[6] = {"aX", "aY", "aZ", "gX", "gY", "gZ"};
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
 
-void setup()
-{
-	Serial.begin(9600);
-	digitalWrite(13, HIGH);
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Color View Test!");
+
+  if (tcs.begin()) {
+    Serial.println("Found sensor");
+  } else {
+    Serial.println("No TCS34725 found ... check your connections");
+    while (1); // halt!
+  }
+  
+  // use these three pins to drive an LED
+  pinMode(redpin, OUTPUT);
+  pinMode(greenpin, OUTPUT);
+  pinMode(bluepin, OUTPUT);
+  
+  // thanks PhilB for this gamma table!
+  // it helps convert RGB colors to what humans see
+  for (int i=0; i<256; i++) {
+    float x = i;
+    x /= 255;
+    x = pow(x, 2.5);
+    x *= 255;
+      
+    if (commonAnode) {
+      gammatable[i] = 255 - x;
+    } else {
+      gammatable[i] = x;      
+    }
+    //Serial.println(gammatable[i]);
+  }
 }
 
-void loop()
-{
-	//read sensor data
-	for (int i=0; i<sensorCount; i++){
-		sensorVals[i] = analogRead(sensorPins[i]);
-	}
 
-	//write sensor data
-	for (int i=0; i<sensorCount; i++){
-		// Serial.print(pinNames[i]);
-		// Serial.print(":");
-		Serial.print(sensorVals[i]);
-		if (i < sensorCount - 1) {
-			Serial.print(", ");
-		}
-		else Serial.print("\n");
-	}
+void loop() {
+  uint16_t clear, red, green, blue;
+
+  tcs.setInterrupt(false);      // turn on LED
+
+  delay(60);  // takes 50ms to read 
+  
+  tcs.getRawData(&red, &green, &blue, &clear);
+
+  tcs.setInterrupt(true);  // turn off LED
+  
+  Serial.print("C:\t"); Serial.print(clear);
+  Serial.print("\tR:\t"); Serial.print(red);
+  Serial.print("\tG:\t"); Serial.print(green);
+  Serial.print("\tB:\t"); Serial.print(blue);
+
+  // Figure out some basic hex code for visualization
+  uint32_t sum = clear;
+  float r, g, b;
+  r = red; r /= sum;
+  g = green; g /= sum;
+  b = blue; b /= sum;
+  r *= 256; g *= 256; b *= 256;
+  Serial.print("\t");
+  Serial.print((int)r, HEX); Serial.print((int)g, HEX); Serial.print((int)b, HEX);
+  Serial.println();
+
+  //Serial.print((int)r ); Serial.print(" "); Serial.print((int)g);Serial.print(" ");  Serial.println((int)b );
+
+  analogWrite(redpin, gammatable[(int)r]);
+  analogWrite(greenpin, gammatable[(int)g]);
+  analogWrite(bluepin, gammatable[(int)b]);
 }
+
